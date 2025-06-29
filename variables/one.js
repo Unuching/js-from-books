a8c1e632260670e47660c2a3ddeb435685c0456d
Unuching Marma <unuching.marma2023@gmail.com>
2025-06-29 11:52:58 +0600

    add new code to this folder

---
+++ variables/one.js
@@ -0,0 +1,10 @@
+const myBirthday = "11 February 1992";
+const numberOfSiblings = 3;
+const numberOfStudents = 30;
+const marks = 7;
+const rooms = 5;
+const studyHour = 5;
+const phoneNumber = 1 + 8 + 3 + 2 + 3 + 4 + 6 + 6 + 5 + 1;
+const total = phoneNumber
+console.log(total );
+
---
+++ variables/two.js
@@ -0,0 +1,19 @@
+const myAge = 32;
+console.log(myAge);
+
+const booksToRead = 6;
+console.log(booksToRead);
+
+const wakeUpTime = "6 AM";
+console.log(wakeUpTime);
+
+const wastedTime = "4 Hour";
+console.log(wastedTime);
+
+const borrow = 500000;
+console.log(borrow);
+
+const numberOfGoals = 4;
+console.log(numberOfGoals);
+const numberOfPillow = 2;
+console.log(numberOfPillow);
